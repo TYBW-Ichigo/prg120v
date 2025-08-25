e5e1a1559562724e983e5b788cd28462e11f3033
TYBW-Ichigo <65979633+TYBW-Ichigo@users.noreply.github.com>
2025-08-25 13:41:11 +0200

    Update eksempel-2.php

--- eksempel-2.php
+++ eksempel-2.php
@@ -9,6 +9,6 @@ $sum=$_POST ["tall1" + "tall2"];
 
 print ("Det første tallet du skrev inn var: $tall1") <br />; 
 print ("Det andre tallet du skrev inn var: $tall2") <br />; 
-print ("Det første tallet du skrev inn var: $tall1") <br />; 
+print ("Summen av tallene du tastet inn er: $sum") <br />; 
 
 ?>
