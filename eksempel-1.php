c815912af8763619ab64aaf91c3cfa40ebf125ce
TYBW-Ichigo <65979633+TYBW-Ichigo@users.noreply.github.com>
2025-08-25 11:37:59 +0200

    Update eksempel-1.php

--- eksempel-1.php
+++ eksempel-1.php
@@ -3,3 +3,7 @@
 /* Programmet mottar 2 navn fra et HTML-skjema ved POST-metoden
 /* Programmet skriver ut navnene 
 */
+$navn1=$_POST ["navn1"];
+$navn2=$_POST ["navn2"];
+
+print ("Navnet du har skrevet inn er $navn1, $navn2");
