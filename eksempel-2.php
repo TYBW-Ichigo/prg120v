b48f00b40b554ce9d00945cbb0b4727c86294c87
TYBW-Ichigo <65979633+TYBW-Ichigo@users.noreply.github.com>
2025-08-25 13:30:57 +0200

    Update eksempel-2.php

--- eksempel-2.php
+++ eksempel-2.php
@@ -1,1 +1,11 @@
+<?php /* Eksempel 1 */
+/*
+/* Programmet mottar 2 tall fra et HTML-skjema ved POST-metoden
+/* Programmet skriver ut tallene, summen og differansen
+*/
+$fornavn=$_POST ["fornavn"];
+$etternavn=$_POST ["etternavn"];
 
+print ("Navnet du har skrevet inn er $fornavn, $etternavn");
+
+?>
