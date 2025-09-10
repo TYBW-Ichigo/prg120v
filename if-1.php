f495e8896790944921eea49a5ac17be35796b5d9
TYBW-Ichigo <65979633+TYBW-Ichigo@users.noreply.github.com>
2025-09-10 10:25:52 +0200

    Create if-1.php

---
+++ if-1.php
@@ -0,0 +1,13 @@
+<?php
+
+$svar=$_POST ["svar"];
+
+  if ($svar == 9)
+  {
+    print("Dette svaret er riktig, da 3 ganger 3 er 9.");
+  }
+  else
+  {
+    print("Dette svaret er ikke riktig, 3 ganger 3 er ikke likt det du skrev inn...");
+  }
+?>
